a56ab9b58dbccef265182a9125c4fc9807d83000
rohitwithcode <radharani7253@gmail.com>
2023-09-26 12:57:33 +0530

    Question 3 added

---
+++ src/Chapter_1_Variables_and_DataTypes/Question_3.java
@@ -0,0 +1,14 @@
+package Chapter_1_Variables_and_DataTypes;
+
+import java.util.Scanner;
+
+public class Question_3 {
+    public static void main(String[] args) {
+
+//        ******** Integer valid or not program
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        System.out.println(scanner.hasNextInt());
+    }
+}
