6bced74546e46aeffed184b31b036e08cfeb403c
rohitwithcode <radharani7253@gmail.com>
2023-09-26 12:27:46 +0530

    Quick Quiz added

--- src/Chapter_1_Variables_and_DataTypes/Quick_Quiz_Add_Three_Numbers.java
+++ src/Chapter_1_Variables_and_DataTypes/Quick_Quiz_Add_Three_Numbers.java
@@ -4,6 +4,7 @@ public class Quick_Quiz_Add_Three_Numbers {
     public static void main(String[] args) {
         int a=3,b=4,c=6;
         int sum=a+b+c;
+
         System.out.println(sum);
     }
 }
