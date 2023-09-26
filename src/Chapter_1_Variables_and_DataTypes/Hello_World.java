c5da4f007b3c925f5e5102bf8819b9d32957717e
rohitwithcode <radharani7253@gmail.com>
2023-09-26 11:30:43 +0530

    added quick quiz

--- src/Chapter_1_Variables_and_DataTypes/Hello_World.java
+++ src/Chapter_1_Variables_and_DataTypes/Hello_World.java
@@ -1,3 +1,5 @@
+package Chapter_1_Variables_and_DataTypes;
+
 public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ src/Chapter_1_Variables_and_DataTypes/Quick_Quiz_Add_Three_Numbers.java
@@ -0,0 +1,9 @@
+package Chapter_1_Variables_and_DataTypes;
+
+public class Quick_Quiz_Add_Three_Numbers {
+    public static void main(String[] args) {
+        int a=3,b=4,c=6;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
