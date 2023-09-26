1fe438229f450a81e843ce10160605698ceb0180
rohitwithcode <radharani7253@gmail.com>
2023-09-26 07:07:47 +0530

    First Commit

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,6 +1,7 @@
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("Welcome Coder Rohit Plateform");
+
         System.out.println("Hello World");
+        System.out.println("Welcome Coder Rohit Plateform");
     }
 }
