9b45d129930941c9779bac4e430a0a8ace107bfa
rohitwithcode <radharani7253@gmail.com>
2023-09-26 11:49:02 +0530

    added quick quiz

---
+++ src/Chapter_1_Variables_and_DataTypes/Literals.java
@@ -0,0 +1,30 @@
+package Chapter_1_Variables_and_DataTypes;
+
+public class Literals {
+    public static void main(String[] args) {
+
+        // ******   Literels Program  ******
+
+        byte age=34;
+        int age1=45;
+        long ageCamel=4555553535553534L;
+        short s=43;
+        char ch='A';
+        float f=4.5f;
+        double d=4.556D;
+        boolean b=true;
+        System.out.println(age);
+        System.out.println(ageCamel);
+        System.out.println(age1);
+        System.out.println(s);
+        System.out.println(ch);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+
+//        ********* String Leteral ********
+
+        String str="Rohit";
+        System.out.println(str);
+    }
+}
