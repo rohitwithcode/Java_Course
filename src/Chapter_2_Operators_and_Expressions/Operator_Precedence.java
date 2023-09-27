5b4261b128970bf13b2dfe6edc69b5aa55e6f41a
rohitwithcode <radharani7253@gmail.com>
2023-09-27 06:12:10 +0530

    Operators Precdece

---
+++ src/Chapter_2_Operators_and_Expressions/Operator_Precedence.java
@@ -0,0 +1,11 @@
+package Chapter_2_Operators_and_Expressions;
+
+public class Operator_Precedence {
+    public static void main(String[] args) {
+        int a=4*3-45/3;
+        System.out.println(a);
+
+        int b=4/3-45*3;
+        System.out.println(b);
+    }
+}
