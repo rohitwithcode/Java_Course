367340abbe95956468cb54de7f8b9607b7aa44f4
rohitwithcode <radharani7253@gmail.com>
2023-09-27 06:28:31 +0530

    Qiuck Quiz added

---
+++ src/Chapter_2_Operators_and_Expressions/Quick_Quiz.java
@@ -0,0 +1,21 @@
+package Chapter_2_Operators_and_Expressions;
+
+public class Quick_Quiz {
+    public static void main(String[] args) {
+        int x=3;
+        int y=4;
+        System.out.println("x-y/2="+(x-y)/2);
+
+        int b=3;
+        int a=5;
+        float c=2;
+        System.out.println("b^2-4ac/2a="+((b*b -(4*a*c))/(2*a)));
+
+        int v=5;
+        int u=3;
+        System.out.println("v^v-u^u="+(v*v - u*u));
+
+        int d=4;
+        System.out.println("a*b-d="+(a*b-d));
+    }
+}
