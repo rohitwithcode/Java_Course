ea18668c27a0fd55e8fe3985d967ec3769f8532b
rohitwithcode <radharani7253@gmail.com>
2023-09-27 06:43:52 +0530

    Quick Quiz 2 added

---
+++ src/Chapter_2_Operators_and_Expressions/Quick_Quiz_2.java
@@ -0,0 +1,10 @@
+package Chapter_2_Operators_and_Expressions;
+
+public class Quick_Quiz_2 {
+    public static void main(String[] args) {
+
+        int y=4;
+        int x=++y*6;
+        System.out.println(x);
+    }
+}
