266b6af71d09f0a03fd29e90430bed44b8409a01
rohitwithcode <radharani7253@gmail.com>
2023-09-29 14:39:48 +0530

    if else added

---
+++ src/Chapter_4_Conditionals/If_Else.java
@@ -0,0 +1,14 @@
+package Chapter_4_Conditionals;
+
+public class If_Else {
+    public static void main(String[] args) {
+
+        int age=24;
+        if (age>18){
+            System.out.println("You can drive");
+        }
+        else {
+            System.out.println("You can't Drive");
+        }
+    }
+}
