396fef39bc00a3805584b2ac3805e06187073972
rohitwithcode <radharani7253@gmail.com>
2023-09-29 14:51:56 +0530

    if else with logical operator added

---
+++ src/Chapter_4_Conditionals/If_else_with_logical_operator.java
@@ -0,0 +1,39 @@
+package Chapter_4_Conditionals;
+
+public class If_else_with_logical_operator {
+    public static void main(String[] args) {
+
+//        System.out.println("For Logical AND ");
+//        boolean t=true;
+//        boolean f=false;
+//
+//        if (t && f){
+//            System.out.println("Yes");
+//        }
+//        else {
+//            System.out.println("No");
+//        }
+
+//        System.out.println("For Logical OR ");
+//        boolean t1=true;
+//        boolean f1=false;
+//
+//        if (t1 || f1){
+//            System.out.println("Yes");
+//        }
+//        else {
+//            System.out.println("No");
+//        }
+
+        System.out.println("For Logical NOT ");
+        boolean t2=true;
+        boolean f2=false;
+
+        if (!(t2 || f2)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
