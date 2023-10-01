a5ab751bd8a4f2ec9f64eac48519e7c7a4e16d21
rohitwithcode <radharani7253@gmail.com>
2023-10-01 11:08:19 +0530

    While Loop Quick Quiz added

---
+++ src/Chapter_5_Loop_Control_Instruction/While_Loop_Quick_Quiz.java
@@ -0,0 +1,12 @@
+package Chapter_5_Loop_Control_Instruction;
+
+public class While_Loop_Quick_Quiz {
+    public static void main(String[] args) {
+        int i=100;
+
+        while (i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
