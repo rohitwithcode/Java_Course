b7acb51185bec4c817363511b8b9f5667a2ce1e3
rohitwithcode <radharani7253@gmail.com>
2023-10-03 21:20:18 +0530

    Recursion Fibonacci series program added

--- src/Chapter_7_Methods_and_Recursion/Recursion_Fib_Series.java
+++ src/Chapter_7_Methods_and_Recursion/Recursion_Fib_Series.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Recursion_Fib_Series {
     static  int fib(int n)
     {
-        if(n==0){
+        if(n==1){
             return 0;
         }
-        if (n==1){
+        if (n==2){
             return 1;
         }
         else {
