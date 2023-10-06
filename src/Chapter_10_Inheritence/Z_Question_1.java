fd26025e6d32eba8593426acf918824ba4a9fdbc
rohitwithcode <radharani7253@gmail.com>
2023-10-06 11:52:36 +0530

    Question 1 added

--- src/Chapter_10_Inheritence/Z_Question_1.java
+++ src/Chapter_10_Inheritence/Z_Question_1.java
@@ -1,32 +1,34 @@
 package Chapter_10_Inheritence;
 
 class Circle{
-
     int r;
-
-    public float Area(){
-        float a= 3.14f*r*r;
-        return a;
+    Circle(int r){
+        this.r=r;
+    }
+    public double Area(){
+//        float a= 3.14f*r*r;
+//        return a;
+        return Math.PI*this.r*this.r;
     }
 }
-
 class Cylinder extends Circle{
-    int r1,h;
-
+    int h;
+    Cylinder(int r,int h){
+        super(r);
+        this.h=h;
+    }
     public float cylinderArea(){
         float a1=3.14f*r*r*h;
         return a1;
+//        return Math.PI*this.r*this.r*this.h;
     }
 }
 public class Z_Question_1 {
     public static void main(String[] args) {
 
-        Cylinder cylinder=new Cylinder();
-        cylinder.h=45;
-        cylinder.r1=6;
-        cylinder.r=3;
+        Cylinder cylinder=new Cylinder(5,6);
 
-        float area=cylinder.Area();
+        double area=cylinder.Area();
         System.out.println("Area of circle is :"+area);
 
         float areaCylinder=cylinder.cylinderArea();
