2e02c890343e27dd74149a7adc4cef036549a6a7
rohitwithcode <radharani7253@gmail.com>
2023-10-06 11:53:01 +0530

    Question 2 added

---
+++ src/Chapter_10_Inheritence/Z_Question_2.java
@@ -0,0 +1,15 @@
+package Chapter_10_Inheritence;
+
+class  Rectangle{
+
+
+}
+class Cuboid extends Rectangle{
+
+}
+
+public class Z_Question_2 {
+    public static void main(String[] args) {
+
+    }
+}
