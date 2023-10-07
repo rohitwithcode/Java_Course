a66f447f36d4bb594f90d3bd523cda93fd39b107
rohitwithcode <radharani7253@gmail.com>
2023-10-07 22:09:11 +0530

    How to import Packages added

---
+++ src/Chapter_12_Packages/Using_Packages.java
@@ -0,0 +1,10 @@
+//package Chapter_12_Packages;
+
+import CodeWithRohit.Library;
+
+public class Using_Packages {
+    public static void main(String[] args) {
+        System.out.println("Using Packages");
+        Library library=new Library();
+    }
+}
