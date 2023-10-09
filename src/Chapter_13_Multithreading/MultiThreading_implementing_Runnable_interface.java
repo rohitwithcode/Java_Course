8500417618541d6f5c512580ee9945af71816524
rohitwithcode <radharani7253@gmail.com>
2023-10-09 21:03:14 +0530

    Multithreading implementing Runnable interface added

---
+++ src/Chapter_13_Multithreading/MultiThreading_implementing_Runnable_interface.java
@@ -0,0 +1,41 @@
+package Chapter_13_Multithreading;
+
+class  ThreadRunnable1 implements Runnable{
+
+    @Override
+    public void run() {
+        int i=1;
+        while (i<=100){
+            System.out.println("I am Runnable interface 1");
+            i++;
+        }
+
+    }
+}
+class  ThreadRunnable2 implements Runnable{
+
+    @Override
+    public void run() {
+        int i=1;
+        while (i<=100){
+            System.out.println("I am Runnable interface 2");
+            i++;
+        }
+
+    }
+}
+
+public class MultiThreading_implementing_Runnable_interface {
+    public static void main(String[] args) {
+
+        ThreadRunnable1 bullet1=new ThreadRunnable1();
+        ThreadRunnable2 bullet2=new ThreadRunnable2();
+
+        Thread gun1=new Thread(bullet1);
+        Thread gun2=new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();;
+
+    }
+}
