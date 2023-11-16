4b5d0e5989c2f0c3c02859644faf2cc2c3a1851d
rohitwithcode <radharani7253@gmail.com>
2023-11-16 06:46:08 +0530

    CurrentMillisFunction Example added

---
+++ src/Chapter_17_Advance_Java_Collection_and_FrameWork/CurrentMillisFunction_Example.java
@@ -0,0 +1,14 @@
+package Chapter_17_Advance_Java_Collection_and_FrameWork;
+
+import static java.lang.System.currentTimeMillis;
+
+public class CurrentMillisFunction_Example {
+    public static void main(String[] args) {
+        System.out.println(currentTimeMillis()); // System.currentTimeMillis() return milliSeconds
+        System.out.println("The Total seconds since 1970 are : "+currentTimeMillis()/1000);
+        System.out.println("The Total minutes since 1970 are : "+currentTimeMillis()/1000/60);
+        System.out.println("The Total hours since 1970 are : "+currentTimeMillis()/1000/3600);
+        System.out.println("The Total days since 1970 are : "+currentTimeMillis()/1000/3600/24);
+        System.out.println("The Total Years from 1970 are : "+currentTimeMillis()/1000/60/60/24/365);
+    }
+}
